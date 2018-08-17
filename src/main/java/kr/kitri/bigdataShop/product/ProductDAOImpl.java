0e942a1ef15db95a12982390d77886a535f6d7c9
kitri <kitri@DESKTOP-F6L3OQC>
2018-08-17 17:46:07 +0900

    180817

---
+++ src/main/java/kr/kitri/bigdataShop/product/ProductDAOImpl.java
@@ -0,0 +1,38 @@
+package kr.kitri.bigdataShop.product;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("productdao")
+public class ProductDAOImpl implements ProductDAO {
+
+	@Autowired
+	SqlSession sqlSession; // dao연동하는 메소드가 정의되어 있는 템플릿 클래스
+	
+	@Override
+	public List<ProductDTO> productlist(String category) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductDTO> hitproduct() {
+		return sqlSession.selectList("kr.kitri.bigdataShop.product.hit");
+	}
+
+	@Override
+	public List<ProductDTO> newproduct() {
+		return sqlSession.selectList("kr.kitri.bigdataShop.product.new");
+
+	}
+
+	@Override
+	public ProductDTO read(String prd_no) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/main/java/main/IndexController.java
+++ src/main/java/main/IndexController.java
@@ -1,15 +1,38 @@
 package main;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import kr.kitri.bigdataShop.product.*;
 
 @Controller
 public class IndexController{
+	
+	@Autowired
+	ProductService service;
+	
 	@RequestMapping("/index.do")
-	public String main(){
+	public ModelAndView main(){
 		System.out.println("어노테이션 기반");
-		return "index";
+		//hit상품과 new상품을 조회하는 서비스의 메소드를 호출하고
+		//ModelAndView에 hit상품과 new상품을 attribute로 저장
+		
+		ModelAndView mav = new ModelAndView();
+		List<ProductDTO> hitproduct = null;
+		hitproduct = service.hitproduct();
+		List<ProductDTO> newproduct = null;
+		newproduct = service.newproduct();
+		
+		mav.addObject("hitproduct", hitproduct);
+		mav.addObject("newproduct", newproduct);
+
+		return mav;
 	}
+	
 	@RequestMapping("/admin/index.do")
 	public String adminmain(){
 		System.out.println("관리자 어노테이션 기반");
