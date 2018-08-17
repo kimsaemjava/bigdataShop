3c9ac31aeb6efd66400f0b6b64986a02c5583990
kitri <kitri@DESKTOP-F6L3OQC>
2018-08-17 17:52:09 +0900

    180817 쇼핑몰 메인

--- src/main/java/kr/kitri/bigdataShop/product/ProductServiceImpl.java
+++ src/main/java/kr/kitri/bigdataShop/product/ProductServiceImpl.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-
+import org.springframework.stereotype.Service;
+@Service
 public class ProductServiceImpl implements ProductService {
 
 	@Autowired
