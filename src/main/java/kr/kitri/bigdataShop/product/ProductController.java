ed72d5466af6f956f857f53a8db6320570a55720
dy <dy@211.226.94.169>
2018-08-22 01:54:53 +0900

    20180821~22/product에 comment만들기 중

--- src/main/java/kr/kitri/bigdataShop/product/ProductController.java
+++ src/main/java/kr/kitri/bigdataShop/product/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.Cookie;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProductController {
 	@Autowired
+	@Qualifier("productservice")
 	ProductService service;
 	
 	//전체상품보기, 카테고리별 보기
--- src/main/java/kr/kitri/bigdataShop/product/ProductServieImpl.java
+++ src/main/java/kr/kitri/bigdataShop/product/ProductServieImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 
-@Service
+@Service("productservice")
 public class ProductServieImpl implements ProductService {
 
 	@Autowired
